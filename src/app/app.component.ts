32eca21c811a13784380fcd03e33d9053bb05d72
顾姗6092001913 <6092001913@zte.intra>
2017-07-07 14:30:15 +0800

    real2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,10 @@ export class AppComponent {
     {label: TimeService.getFormatDate(this.beginDate, TimeGr.date), closable: false},
     {label: TimeService.getFormatDate(this.endDate, TimeGr.date), closable: false}
   ]);
+  periodTimes = [{label: '1', closable: false}, {label: '2', closable: false}, {label: '3', closable: false},
+    {label: '4', closable: false}, {label: '5', closable: false}, {label: '6', closable: false},
+    {label: '7', closable: false}, {label: '8', closable: false}, {label: '9', closable: false},
+    {label: '10', closable: false}, {label: '11', closable: false}, {label: '12', closable: false}];
 
   constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
   }
