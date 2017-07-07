6e4f86c49a7df117cd2dc82c29363d0f2b002b5e
顾姗6092001913 <6092001913@zte.intra>
2017-07-07 14:53:22 +0800

    real3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,26 @@ export class AppComponent {
     {label: '7', closable: false}, {label: '8', closable: false}, {label: '9', closable: false},
     {label: '10', closable: false}, {label: '11', closable: false}, {label: '12', closable: false}];
 
+  quickChoices = [{label: '一天', id: '1'}, {label: '三天', id: '2'}, {label: '七天', id: '3'}];
+
+  selectedChoice = this.quickChoices[0];
+
   constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
+  }
+  quickChoiceChange(quickChoice) {
+    switch (quickChoice.id) {
+      case '1':
+        this.beginDate = 'now-1d';
+        break;
+      case '2':
+        this.beginDate = 'now-3d';
+        break;
+      case '3':
+        this.beginDate = 'now-1w';
+        break;
+    }
+    this.endDate = 'now';
+    this.handleChange();
   }
 
   handleChange() {
